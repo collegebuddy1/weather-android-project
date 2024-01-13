bd6160df2cde4e820b5ee3973c834be41d4807ee
ritik-prog <ritikmakhija2002@gmail.com>
2024-01-13 12:09:55 +0530

    Initial commit

---
+++ app/src/main/java/org/woheller69/weather/dialogs/AddLocationDialogPhotonAPI.java
@@ -0,0 +1,288 @@
+package org.woheller69.weather.dialogs;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.os.ConfigurationCompat;
+import androidx.fragment.app.DialogFragment;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.woheller69.weather.BuildConfig;
+import org.woheller69.weather.R;
+import org.woheller69.weather.activities.ManageLocationsActivity;
+import org.woheller69.weather.database.City;
+import org.woheller69.weather.database.PFASQLiteHelper;
+import org.woheller69.weather.ui.util.photonApiCall;
+import org.woheller69.weather.ui.util.AutoSuggestAdapter;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+public class AddLocationDialogPhotonAPI extends DialogFragment {
+
+    Activity activity;
+    View rootView;
+    PFASQLiteHelper database;
+    private WebView webview;
+
+    private AutoCompleteTextView autoCompleteTextView;
+    City selectedCity;
+
+    private static final int TRIGGER_AUTO_COMPLETE = 100;
+    private static final long AUTO_COMPLETE_DELAY = 300;
+    private static final int TRIGGER_HIDE_KEYBOARD = 200;
+    private static final long HIDE_KEYBOARD_DELAY = 3000;
+    private Handler handler;
+    private AutoSuggestAdapter autoSuggestAdapter;
+    String url="https://photon.komoot.io/api/?q=";
+    String lang="default";
+
+    public AddLocationDialogPhotonAPI() {
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+       if (context instanceof Activity){
+            this.activity=(Activity) context;
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) dismiss();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handler.removeMessages(TRIGGER_HIDE_KEYBOARD);
+        if(selectedCity != null && webview != null) webview.loadUrl("file:///android_asset/map.html?lat=" + selectedCity.getLatitude() + "&lon=" + selectedCity.getLongitude());
+    }
+
+    @NonNull
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Locale locale = ConfigurationCompat.getLocales(Resources.getSystem().getConfiguration()).get(0);
+        //supported languages by photon.komoot.io API: default, en, de, fr, it
+        if ((locale.getLanguage().equals("de"))||(locale.getLanguage().equals("en"))||(locale.getLanguage().equals("fr"))||(locale.getLanguage().equals("it")))                 {
+            lang=locale.getLanguage();
+        } else {
+            lang="default";
+        }
+
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = inflater.inflate(R.layout.dialog_add_location, null);
+
+        rootView = view;
+
+        builder.setView(view);
+        builder.setTitle(getActivity().getString(R.string.dialog_add_label));
+
+        this.database = PFASQLiteHelper.getInstance(getActivity());
+
+
+        webview= rootView.findViewById(R.id.webViewAddLocation);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setUserAgentString(BuildConfig.APPLICATION_ID+"/"+BuildConfig.VERSION_NAME);
+        webview.setBackgroundColor(0x00000000);
+        webview.setBackgroundResource(R.drawable.photon);
+
+        final TextView textview = rootView.findViewById(R.id.textViewAddLocation);
+        textview.setVisibility(View.GONE);
+
+        autoCompleteTextView = (AutoCompleteTextView) rootView.findViewById(R.id.autoCompleteTvAddDialog);
+
+        //Setting up the adapter for AutoSuggest
+        autoSuggestAdapter = new AutoSuggestAdapter(requireContext(),
+                R.layout.list_item_autocomplete);
+        autoCompleteTextView.setThreshold(2);
+        autoCompleteTextView.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        autoCompleteTextView.setAdapter(autoSuggestAdapter);
+
+        autoCompleteTextView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        selectedCity=autoSuggestAdapter.getObject(position);
+                        //Hide keyboard to have more space
+                        final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
+                        //Show city on map
+                        webview.loadUrl("file:///android_asset/map.html?lat=" + selectedCity.getLatitude() + "&lon=" + selectedCity.getLongitude());
+                    }
+                });
+
+        autoCompleteTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int
+                    count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                handler.removeMessages(TRIGGER_AUTO_COMPLETE);
+                handler.sendEmptyMessageDelayed(TRIGGER_AUTO_COMPLETE, AUTO_COMPLETE_DELAY);
+                handler.removeMessages(TRIGGER_HIDE_KEYBOARD);
+                handler.sendEmptyMessageDelayed(TRIGGER_HIDE_KEYBOARD, HIDE_KEYBOARD_DELAY);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        handler = new Handler(Looper.getMainLooper(), msg -> {
+            if (msg.what == TRIGGER_AUTO_COMPLETE) {
+                if (!TextUtils.isEmpty(autoCompleteTextView.getText())) {
+                    try {
+                        makeApiCall(URLEncoder.encode(autoCompleteTextView.getText().toString(), StandardCharsets.UTF_8.name()));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (msg.what == TRIGGER_HIDE_KEYBOARD) {
+                //Hide keyboard to show entries behind the keyboard
+                final InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
+            }
+            return false;
+        });
+
+        builder.setPositiveButton(getActivity().getString(R.string.dialog_add_add_button), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                performDone();
+            }
+        });
+
+        builder.setNegativeButton(getActivity().getString(R.string.dialog_add_close_button), null);
+
+        return builder.create();
+
+    }
+    private void makeApiCall(String text) {
+        photonApiCall.make(getContext(), text, url,lang, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //parsing logic, please change it as per your requirement
+                List<String> stringList = new ArrayList<>();
+                List<City> cityList = new ArrayList<>();
+                try {
+                    JSONObject responseObject = new JSONObject(response);
+                    JSONArray array = responseObject.getJSONArray("features");
+                    for (int i = 0; i < array.length(); i++) {
+                        City city =new City();
+                        String citystring="";
+                        JSONObject jsonFeatures = array.getJSONObject(i);
+                        JSONObject jsonProperties = jsonFeatures.getJSONObject("properties");
+                        JSONObject jsonGeometry=jsonFeatures.getJSONObject("geometry");
+                        JSONArray jsonCoordinates=jsonGeometry.getJSONArray("coordinates");
+                        String name="";
+                        if (jsonProperties.has("name")) {
+                            name=jsonProperties.getString("name");
+                            citystring=citystring+name+", ";
+                        }
+                        String postcode="";
+                        if (jsonProperties.has("postcode")) {
+                            postcode=jsonProperties.getString("postcode");
+                            citystring=citystring+postcode+", ";
+                        }
+                        String cityname=name;
+                        if (jsonProperties.has("city")) {
+                            cityname=jsonProperties.getString("city");
+                            citystring=citystring+cityname+", ";
+                        }
+                        String state="";
+                        if (jsonProperties.has("state")) {
+                            state=jsonProperties.getString("state");
+                            citystring=citystring+state+", ";
+                        }
+                        String countrycode="";
+                        if (jsonProperties.has("countrycode")) {
+                            countrycode=jsonProperties.getString("countrycode");
+                            citystring=citystring+countrycode;
+                        }
+
+                        city.setCityName(cityname);
+                        city.setCountryCode(countrycode);
+                        city.setLatitude((float) jsonCoordinates.getDouble(1));
+                        city.setLongitude((float) jsonCoordinates.getDouble(0));
+                        cityList.add(city);
+                        stringList.add(citystring);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //IMPORTANT: set data here and notify
+                autoSuggestAdapter.setData(stringList,cityList);
+                autoSuggestAdapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Handler h = new Handler(activity.getMainLooper());
+                h.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(activity, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+    }
+
+
+    private void performDone() {
+        if (selectedCity == null) {
+            Toast.makeText(activity, R.string.dialog_add_no_city_found, Toast.LENGTH_SHORT).show();
+        }else {
+            ((ManageLocationsActivity) activity).addCityToList(selectedCity);
+            dismiss();
+        }
+    }
+
+}
---
+++ app/src/main/java/org/woheller69/weather/ui/viewPager/WeatherPagerAdapter.java
@@ -0,0 +1,125 @@
+package org.woheller69.weather.ui.viewPager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import org.woheller69.weather.database.CityToWatch;
+import org.woheller69.weather.database.CurrentWeatherData;
+import org.woheller69.weather.database.Forecast;
+import org.woheller69.weather.database.PFASQLiteHelper;
+import org.woheller69.weather.database.WeekForecast;
+import org.woheller69.weather.services.UpdateDataService;
+import org.woheller69.weather.ui.WeatherCityFragment;
+import org.woheller69.weather.ui.updater.IUpdateableCityUI;
+
+import java.util.Collections;
+import java.util.List;
+
+import static androidx.core.app.JobIntentService.enqueueWork;
+import static org.woheller69.weather.services.UpdateDataService.SKIP_UPDATE_INTERVAL;
+import static org.woheller69.weather.ui.RecycleList.CityWeatherAdapter.CHART;
+import static org.woheller69.weather.ui.RecycleList.CityWeatherAdapter.DAY;
+import static org.woheller69.weather.ui.RecycleList.CityWeatherAdapter.DETAILS;
+import static org.woheller69.weather.ui.RecycleList.CityWeatherAdapter.EMPTY;
+import static org.woheller69.weather.ui.RecycleList.CityWeatherAdapter.OVERVIEW;
+import static org.woheller69.weather.ui.RecycleList.CityWeatherAdapter.WEEK;
+
+/**
+ * Created by thomagglaser on 07.08.2017.
+ */
+
+public class WeatherPagerAdapter extends FragmentStateAdapter implements IUpdateableCityUI {
+
+    private Context mContext;
+
+    private PFASQLiteHelper database;
+
+    private List<CityToWatch> cities;
+
+
+    public WeatherPagerAdapter(Context context, @NonNull FragmentManager supportFragmentManager, @NonNull Lifecycle lifecycle) {
+        super(supportFragmentManager,lifecycle);
+        this.mContext = context;
+        this.database = PFASQLiteHelper.getInstance(context);
+
+        loadCities();
+    }
+
+    public void loadCities() {
+        this.cities = database.getAllCitiesToWatch();
+        Collections.sort(cities, (o1, o2) -> o1.getRank() - o2.getRank());
+    }
+
+    @NonNull
+    @Override
+    public WeatherCityFragment createFragment(int position) {
+        Bundle args = new Bundle();
+        args.putInt("city_id", cities.get(position).getCityId());
+
+        return WeatherCityFragment.newInstance(args);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cities.size();
+    }
+
+    public CharSequence getPageTitle(int position) {
+         return cities.get(position).getCityName();
+    }
+
+    public static void refreshSingleData(Context context, Boolean asap, int cityId) {
+        Intent intent = new Intent(context, UpdateDataService.class);
+        intent.setAction(UpdateDataService.UPDATE_SINGLE_ACTION);
+        intent.putExtra(SKIP_UPDATE_INTERVAL, asap);
+        intent.putExtra("cityId",cityId);
+        enqueueWork(context, UpdateDataService.class, 0, intent);
+    }
+
+
+    @Override
+    public void processNewCurrentWeatherData(CurrentWeatherData data) {
+
+    }
+
+    @Override
+    public void processNewForecasts(List<Forecast> forecasts) {
+        //empty because Fragments are subscribers themselves
+    }
+
+    @Override
+    public void processNewWeekForecasts(List<WeekForecast> forecasts) {
+        //empty because Fragments are subscribers themselves
+    }
+
+    public int getCityIDForPos(int pos) {
+            CityToWatch city = cities.get(pos);
+                 return city.getCityId();
+    }
+
+    public int getPosForCityID(int cityID) {
+        for (int i = 0; i < cities.size(); i++) {
+            CityToWatch city = cities.get(i);
+            if (city.getCityId() == cityID) {
+                return i;
+            }
+        }
+        return -1;  //item not found
+    }
+
+    public float getLatForPos(int pos) {
+        CityToWatch city = cities.get(pos);
+        return city.getLatitude();
+    }
+
+    public float getLonForPos(int pos) {
+        CityToWatch city = cities.get(pos);
+        return city.getLongitude();
+    }
+
+}
